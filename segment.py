8bf803ff3e116ecf18c2e292d8280e6baa7c580e
elitrout <elitrout24@gmail.com>
2015-03-10 15:13:21 +0100

    Create segment.py

---
+++ segment.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Get 30-second excerpt in the middle of the song. Flac to Wav conversion using ffmpeg.
+
+"""
+
+import os
+from scipy.io import wavfile
+
+audioDir = "./dataset"
+outputDir = "./output"
+os.mkdir(outputDir)
+
+segLen = 30
+segLenH = segLen / 2
+
+for dirName, subdirList, fileList in os.walk(audioDir):
+    print('Processing directory: %s' % dirName)
+
+    os.mkdir(outputDir + '/' + dirName[2: ])
+    i = 0
+    for fname in fileList:
+        if i >= 5:
+            break
+        if fname[-5: ] == '.flac':
+            inputFlac = os.path.join(dirName[2: ], fname)
+            outputWav = os.path.join(outputDir, dirName[2: ] + '/' + fname[: -5] + '.wav')
+
+            # convert .Flac to .Wav using ffmpeg
+            os.system('ffmpeg -i "' + inputFlac + '" "' + outputWav + '"')
+
+            # segment file
+            fs, x = wavfile.read(outputWav)
+            seg = x[len(x) / 2 - segLenH * fs : len(x) / 2 + segLenH * fs]
+            # this would overwrite file with the segment
+            wavfile.write(outputWav, fs, seg)
+
+            i += 1
+            print('\t%s' % fname)
